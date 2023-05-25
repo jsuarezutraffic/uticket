afd769c528d668d7af515bade21d054c8819f548
jsuarez <jsuarezutraffic>
2023-05-25 16:26:32 -0500

    visualizador de imagenes

--- src/components/VerImagenArray.vue
+++ src/components/VerImagenArray.vue
@@ -1,42 +1,28 @@
 <template>
   <q-card style="width: 50em; max-width: 100%; height: auto">
-    <div class="row" v-if="imagenes.length > 0">
-      <div class="col" v-for="img in imagenes" :key="img">
-        <q-img
-          class="q-gutter-sm q-my-sm"
-          :src="img"
-          alt=""
-          spinner-color="red"
-          outlined
-          fit="scale-down"
-        >
-        </q-img>
-        <br />
-      </div>
+    <div class="row q-pa-md" v-if="imagenes.length > 0">
+      <q-carousel
+        animated
+        v-model="slide"
+        arrows
+        navigation
+        infinite
+        control-color="black"
+        padding
+        style="width: 100%; min-height: 60vh"
+      >
+        <q-carousel-slide
+          v-for="(img, index) in imagenes"
+          :key="index"
+          :name="index"
+          :img-src="img"
+        />
+      </q-carousel>
     </div>
-    <div class="row text-center container" style="height: 10em" v-else>
-      <h4>No existen evidencias</h4>
+    <div class="row text-center justify-center" v-else>
+      <h4 class="col-12">No existen Evidencias</h4>
     </div>
   </q-card>
-  <!-- <q-card style="width: 50em; max-width: 100%; height: auto">
-    <div class="row" v-if="imagenes.length > 0">
-      <div class="col" v-for="img in imagenes" :key="img">
-        <q-img
-          class="q-gutter-sm q-my-sm"
-          :src="img"
-          alt=""
-          spinner-color="red"
-          outlined
-          fit="scale-down"
-        >
-        </q-img>
-        <br />
-      </div>
-    </div>
-    <div class="row text-center container" style="height: 10em" v-else>
-      <h4>No existen evidencias</h4>
-    </div>
-  </q-card> -->
 </template>
 
 <script setup>
@@ -50,62 +36,17 @@ const props = defineProps({
     required: true,
   },
 });
+
+const slide = ref(0);
 const imagenes = ref(props.datoProp.split("|"));
-// if (condition) {
-//   imagenes.value.pop();
-// }
 
-console.log(imagenes.value);
-
-// const DatosExportado = ref("");
-// const loadingImage = ref(false);
-// const filaavatar = ref([]);
-// const archivos = ref([]);
-// const archivos2 = ref("");
-
-// // se ejecuta cuando se carga la imagen
-// watch(filaavatar, (currentValue, oldValue) => {
-//   console.log(filaavatar.value);
-//   archivos2.value = "";
-//   convertirBase64();
-// });
-
-// async function convertirBase64() {
-//   loadingImage.value = true;
-//   if (filaavatar.value.length !== 0) {
-//     async function asyncForEach(array, callback) {
-//       for (const item of array) {
-//         await callback(item);
-//       }
-//     }
-//     await asyncForEach(filaavatar.value, async (item) => {
-//       const file = await createBase64Image(item);
-//       archivos2.value += file + "|";
-//     });
-//     DatosExportado.value = archivos2.value;
-//     archivos.value = archivos2.value.split("|");
-//     archivos.value.pop();
-//   } else {
-//     archivos.value = [];
-//   }
-//   loadingImage.value = false;
-// }
-
-// function clearImage(value) {
-//   const index = archivos.value.indexOf(value);
-//   if (index !== -1) {
-//     archivos.value.splice(index, 1);
-//     filaavatar.value.splice(index, 1);
-//   }
-// }
-
-// Resto del código del componente...
-// const emit = defineEmits(["datos-exportado-cambiado"]);
-
-// // Emitir evento cuando DatosExportado cambia
-// watch(DatosExportado, (currentValue) => {
-//   emit("datos-exportado-cambiado", currentValue);
-// });
+if (imagenes.value[0] == "") {
+  imagenes.value.pop();
+} else if (imagenes.value.length > 1) {
+  imagenes.value.pop();
+} else {
+  console.log("nada");
+}
 </script>
 <style>
 .card-container2 {
