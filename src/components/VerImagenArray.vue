b06452212f2fb9ea2f347c3bb553d206e403f504
jsuarez <jsuarezutraffic>
2023-09-18 14:49:21 -0500

    visualzacion de evidencias en otra pestaña

--- src/components/VerImagenArray.vue
+++ src/components/VerImagenArray.vue
@@ -16,7 +16,21 @@
           :key="index"
           :name="index"
           :img-src="img"
+          ref="carrusel"
         />
+        <template v-slot:control>
+          <q-carousel-control position="bottom-right" :offset="[18, 18]">
+            <q-btn
+              @click="clickImag"
+              push
+              round
+              dense
+              color="white"
+              text-color="primary"
+              icon="fullscreen"
+            />
+          </q-carousel-control>
+        </template>
       </q-carousel>
     </div>
     <div class="row text-center justify-center" v-else>
@@ -38,6 +52,7 @@ const props = defineProps({
 
 const slide = ref(0);
 const imagenes = ref(props.datoProp.split("|"));
+const carrusel = ref();
 
 if (imagenes.value[0] == "") {
   imagenes.value.pop();
@@ -45,6 +60,20 @@ if (imagenes.value[0] == "") {
   imagenes.value.pop();
 } else {
 }
+const clickImag = () => {
+  console.log(carrusel.value[0].imgSrc);
+  var newWindow = window.open("", "_blank");
+  newWindow.document.write(
+    '<html><head><title>Imagen en nueva ventana</title></head><body style="margin: 0; padding: 0; height: 100vh; display: flex; justify-content: center; align-items: center;">'
+  );
+  newWindow.document.write(
+    '<img src="' +
+      carrusel.value[0].imgSrc +
+      '" style="max-width: 100%; max-height: 100%;"/>'
+  );
+  newWindow.document.write("</body></html>");
+  newWindow.document.close();
+};
 </script>
 <style>
 .card-container2 {
